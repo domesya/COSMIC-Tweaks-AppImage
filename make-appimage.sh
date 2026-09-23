04584e6091c25eb29558e30040af4855a504b1a1
domesya <163451587+domesya@users.noreply.github.com>
2026-09-23 16:08:31 +0200

    Enable experimental host drivers in make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,6 +10,7 @@ export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/scalable/apps/dev.edfloreshz.CosmicTweaks.svg
 export DESKTOP=/usr/share/applications/dev.edfloreshz.CosmicTweaks.desktop
+export USE_HOST_DRIVERS_EXPERIMENTAL=1
 
 # Deploy dependencies
 quick-sharun /usr/bin/cosmic-ext-tweaks
